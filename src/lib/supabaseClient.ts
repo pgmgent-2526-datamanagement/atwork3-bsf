15f97b91ee5688d5b778415d2f585eca773bcc4a
kugayumasan <erosferri@hotmail.com>
2025-12-17 09:23:59 +0100

    changed client and server

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserClient } from "@supabase/auth-helpers-nextjs"
+import type { Database } from "@/types/supabase"
 
-export const supabase = createBrowserClient(
+export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+)
