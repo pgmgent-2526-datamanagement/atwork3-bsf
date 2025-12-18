4a6a9e64d34e86c6c734df7bc8c743535745c778
kugayumasan <erosferri@hotmail.com>
2025-12-18 15:57:39 +0100

    created the vote results page and qr for admin using what we made

--- src/app/admin/voting/page.tsx
+++ src/app/admin/voting/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import VotingControl from "@/components/admin/VotingControl/VotingControl";
 
 export default function VotingPage() {
-  const [votingOpenEventHall, setVotingOpenEventHall] = useState(false);
-  const [votingOpenHome, setVotingOpenHome] = useState(false);
-    
-  return (
-    <VotingControl
-      votingOpenEventHall={votingOpenEventHall}
-      setVotingOpenEventHall={setVotingOpenEventHall}
-      votingOpenHome={votingOpenHome}
-      setVotingOpenHome={setVotingOpenHome}
-    />
-  );
+  return <VotingControl />;
 }
