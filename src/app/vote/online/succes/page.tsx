c840e3734f3f0b9b00dd185f5ebb72e3399709fc
«Clash-yabi» <yabesolo@student.arteveldehs.be>
2026-01-15 15:52:47 +0100

    fix: safe err handeling

--- src/app/vote/online/succes/page.tsx
+++ src/app/vote/online/succes/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
 
     (async () => {
       try {
-        if (!filmId) {
+        if (filmId === null) {
           if (mounted) setFilm(null);
           return;
         }
