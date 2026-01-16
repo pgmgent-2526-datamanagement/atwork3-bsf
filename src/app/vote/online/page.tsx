ad110848dfbb6b9483a3b8cc13b20fe9e77fddcf
«Clash-yabi» <yabesolo@student.arteveldehs.be>
2026-01-16 09:58:24 +0100

    change props and url query name

--- src/app/vote/online/page.tsx
+++ src/app/vote/online/page.tsx
@@ -6,8 +6,8 @@ import { useRouter } from "next/navigation";
 export default function Page() {
   const router = useRouter();
 
-  const handleVoteConfirmed = (filmNumber: number) => {
-    router.push(`/vote/online/success?film=${filmNumber}`);
+  const handleVoteConfirmed = (filmId: number) => {
+    router.push(`/vote/online/success?filmId=${filmId}`);
   };
 
   return <VotingPage onVoteConfirmed={handleVoteConfirmed} />;
